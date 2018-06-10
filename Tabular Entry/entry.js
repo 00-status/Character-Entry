6d9ee25eda659c471581ae045d9fad66d521c7ca
Liam Johnson <johnson1882@saskpolytech.ca>
2018-06-09 19:34:56 -0600

    Added the main functionality and prettied up the interface. Very Cross-Site-Scriptable at the momemt. Will have to scrub the user's information.

--- Tabular Entry/entry.js
+++ Tabular Entry/entry.js
@@ -1,1 +1,24 @@
-﻿
+﻿$(function()
+{
+
+    $("#addEntry").click(function() { addEntry() });
+
+    function addEntry()
+    {
+        var nameField = $("#nameField").val()
+        var classField = $("#classField").val()
+        var raceField = $("#raceField").val()
+        var levelField = $("#levelField").val()
+
+        if (nameField != "" && classField != "" && raceField != "" && levelField != "")
+        {
+            var string = "<tr>";
+            string += "<td>" + nameField + "</td>";
+            string += "<td>" + classField + "</td>";
+            string += "<td>" + raceField + "</td>";
+            string += "<td>" + levelField + "</td>";
+            string += "</tr>";
+            $("#characters").append(string);
+        }
+    }
+});
